5bbeaca6cdd7a3c2237ac410abb146ad17153f98
CengizhanBayram <200316066@ogr.cbu.edu.tr>
2024-07-06 15:19:27 +0300

    #Cengizhan Bayram

--- ContourDetection/contour_detection.py
+++ ContourDetection/contour_detection.py
@@ -3,4 +3,5 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 img = cv2.imread("contour.jpg",0)
-plt.figure()
+plt.figure(), plt.imshow(img,cmap="gray"),plt.axis("off")
+cv2.findContours(img , cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
